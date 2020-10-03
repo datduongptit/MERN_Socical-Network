6c758ad5d6be6f1b46ed9ad22e223532443dcd9b
duongdat98 <duongdat@coders.com>
2020-10-03 23:50:43 +0700

    update to deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //Connect database
@@ -9,13 +9,21 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 //define routes
 app.use('/api/users', require('./routes/apis/users'));
 app.use('/api/auth', require('./routes/apis/auth'));
 app.use('/api/profile', require('./routes/apis/profile'));
 app.use('/api/posts', require('./routes/apis/posts'));
+
+// server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
