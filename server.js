40f68bbeac6f00c0f932acc38044d82b666f026e
datduongptit <datduongptit@gmail.com>
2020-11-04 14:43:22 +0700

    hihi

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port  ${PORT}`));
